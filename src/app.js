56ad054983dc1c9ea54166d15f5122a1cc54020d
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2024-05-09 06:07:28 +0000

    firs commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,47 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const cardNumbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "A",
+  "Q",
+  "J",
+  "K"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const cardsSuit = ["♦", "♥", "♠", "♣"];
+
+window.onload = () => {
+  const cardNumber = document.getElementById("card");
+
+  let randomCardSuit = cardsSuit[Math.floor(Math.random() * 4)];
+  let randomCardNumbers = cardNumbers[Math.floor(Math.random() * 12)];
+  const cardBody = document.createElement("div");
+  if (randomCardSuit === "♦" || randomCardSuit === "♥") {
+    cardBody.innerHTML = `
+    <div class="suitUp red"><i class="suitUpImg">${randomCardSuit}</i></div>
+      <div class="cardNumber">
+        <p class="number red">${randomCardNumbers}</p>
+      </div>
+    <div class="suitDown red"><i class="suitDownImg">${randomCardSuit}</i></div>
+  `;
+  } else {
+    cardBody.innerHTML = `
+    <div class="suitUp"><i class="suitUpImg">${randomCardSuit}</i></div>
+      <div class="cardNumber">
+        <p class="number">${randomCardNumbers}</p>
+      </div>
+    <div class="suitDown"><i class="suitDownImg">${randomCardSuit}</i></div>
+  `;
+  }
+
+  cardNumber.appendChild(cardBody);
 };
